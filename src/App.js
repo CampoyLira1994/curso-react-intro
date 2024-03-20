d6f80f078b1398618a7717fa79de2ee851f6617e
CampoyLira1994 <campoy.lira.1994@gmail.com>
2024-03-20 16:07:47 -0400

    Class 4

--- src/App.js
+++ src/App.js
@@ -5,26 +5,40 @@ import { TodoButtonCreate } from './TodoButtonCreate/TodoButtonCreate';
 import { TodoList } from './TodoList/TodoList';
 import { TodoSearch } from './TodoSearch/TodoSearch';
 import { TodoItem } from './TodoItem/TodoItem';
+import React from 'react';
 
+
+const defaultTodo = [
+    { text: 'Test1', completed: true },
+    { text: 'Test2', completed: false },
+    { text: 'Test3', completed: false },
+    { text: 'Test4', completed: false },
+    { text: 'Test5', completed: true },
+  ];
 
 function App() {
   return (
-    <div className="App">
+    // eslint-disable-next-line react/jsx-no-undef
+    <React.Fragment>
 
-    <TodoCounter completed={5} total={20}/>
+    <TodoCounter completed={16} total={25}/>
     <TodoSearch/>
 
     <TodoList>
 
-    <TodoItem/>
-    <TodoItem/>
-    <TodoItem/>
+{defaultTodo.map(todo => (
+  <TodoItem 
+  key={todo.text} 
+  text={todo.text}
+  completed={todo.completed}
+  />
+))}
 
     </TodoList>
 
     <TodoButtonCreate/>
 
-    </div>
+    </React.Fragment>
   );
 }
 
