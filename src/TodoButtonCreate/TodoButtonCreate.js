b895424c87723b942bfda8ba47a9d3472fb86935
CampoyLira1994 <campoy.lira.1994@gmail.com>
2024-03-22 13:31:25 -0400

    Class 6

--- src/TodoButtonCreate/TodoButtonCreate.js
+++ src/TodoButtonCreate/TodoButtonCreate.js
@@ -1,6 +1,12 @@
 function TodoButtonCreate() {
   return (
-    <button>+</button>
+    <button className="CreateTodoButton" onClick={
+      (event) => {
+        console.log('le diste click')
+        console.log(event)
+      }
+    }
+    >+</button>
   );
 }
 
--- src/TodoSearch/TodoSearch.js
+++ src/TodoSearch/TodoSearch.js
@@ -1,6 +1,19 @@
+import { TodoContext } from '../TodoContext';
+import './TodoSearch.css';
+import React from 'react';
+
 function TodoSearch() {
+
+    const [searchValue, setSate] = React.UseState('');
+
     return (
-        <input placeholder="Search" className="TodoSearch" />
+        <input placeholder="Search" className="TodoSearch" value={searchValue}
+            onChange = {(event) => {
+                console.log('TodoSearchConsole')
+                console.log(event)
+                console.log(event.target.value)
+            }}
+        />
     );
   }
   
